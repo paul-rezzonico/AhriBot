2711357d2942781473125ff53c0fd2e1fcea99f7
paul <paul.rezzonico@windows.com>
2023-11-13 16:04:16 +0100

    config(build.gradle) update build.gradle.kts file with gradle annd JDA dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "3.1.5"
 }
+
+apply(plugin = "io.spring.dependency-management")
 
 group = "com.paulrezzonico"
 version = "1.0-SNAPSHOT"
@@ -10,6 +13,10 @@ repositories {
 }
 
 dependencies {
+    implementation("net.dv8tion:JDA:5.0.0-beta.17")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }

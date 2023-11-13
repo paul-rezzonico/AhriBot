39d82782eb06c5c6da343904a478e26df6c5eb36
paul <paul.rezzonico@windows.com>
2023-11-13 16:12:49 +0100

    feat(Main) add spring and jda to start

--- src/main/java/com/paulrezzonico/Main.java
+++ src/main/java/com/paulrezzonico/Main.java
@@ -1,19 +1,22 @@
 package com.paulrezzonico;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class Main {
+
+    @Value("${discord.bot.token}")
+    private String token;
     public static void main(String[] args) {
-        // Press Ctrl+1 with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        // Press Alt+Maj+X or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Alt+Maj+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+Maj+B.
-            System.out.println("i = " + i);
-        }
+        SpringApplication.run(Main.class, args);
+    }
+    @Bean
+    public JDA jda() {
+        return JDABuilder.createDefault(token).build();
     }
 }

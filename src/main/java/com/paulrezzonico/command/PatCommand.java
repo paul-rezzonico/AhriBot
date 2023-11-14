eeb7578147af17672b42a25b20db25ea2a4ebff6
paul <paul.rezzonico@windows.com>
2023-11-14 11:22:13 +0100

    Feat(PatCommand) add pat command with a gif

---
+++ src/main/java/com/paulrezzonico/command/PatCommand.java
@@ -0,0 +1,29 @@
+package com.paulrezzonico.command;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.util.Objects;
+
+@Component
+public class PatCommand extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        EmbedBuilder embed = new EmbedBuilder();
+        if (event.getName().equals("pat")) {
+            String userName = Objects.requireNonNull(event.getOption("user")).getAsUser().getAsMention();
+            String gifUrl =
+                    "https://media.tenor.com/mecnd_qE8p8AAAAd/anime-pat.gif";
+
+            embed.setColor(Color.PINK);
+            embed.setDescription("Patting " + userName);
+            embed.setImage(gifUrl);
+
+            event.replyEmbeds(embed.build()).queue();
+        }
+    }
+}

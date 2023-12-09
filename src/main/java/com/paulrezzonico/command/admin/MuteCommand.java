d42fe9a5033f2c15109c1b47862a4fed83782ce1
paul <paul.rezzonico@windows.com>
2023-12-09 22:06:22 +0100

    Feat(MuteCommand) add a slash command to mute an user

---
+++ src/main/java/com/paulrezzonico/command/admin/MuteCommand.java
@@ -0,0 +1,57 @@
+package com.paulrezzonico.command.admin;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+public class MuteCommand extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if (event.getName().equals("mute")) {
+            Member member = Objects.requireNonNull(event.getOption("user")).getAsMember();
+            long duration = parseDuration(Objects.requireNonNull(event.getOption("duration")).getAsString());
+
+            if (member != null && duration > 0) {
+                Guild guild = event.getGuild();
+                if (guild != null) {
+                    for (GuildChannel channel : guild.getChannels()) {
+                        if (channel instanceof TextChannel textChannel) {
+                            handleMute(textChannel, member, duration, event);
+                        }
+                    }
+
+                    event.reply("Muted " + member.getAsMention() + " for " + duration + " minutes.").queue();
+                }
+            } else {
+                event.reply("User not found or invalid duration.").queue();
+            }
+        }
+    }
+
+    private void handleMute(TextChannel channel, Member member, long duration, SlashCommandInteractionEvent event) {
+        channel.upsertPermissionOverride(member)
+            .deny(Permission.MESSAGE_SEND)
+            .queue();
+
+        event.getJDA().getRateLimitPool().schedule(() -> channel.upsertPermissionOverride(member)
+            .clear(Permission.MESSAGE_SEND)
+            .queue(), duration, TimeUnit.MINUTES);
+    }
+
+
+    private long parseDuration(String durationStr) {
+        try {
+            return Long.parseLong(durationStr);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+}

293f2e1f912233551efc2f0e1b3a559a535f0106
paul-rezzonico <paul.rezzonico@legrand.com>
2025-07-15 14:47:45 +0200

    feat: implement /unmute command to restore user messaging permissions

---
+++ src/main/java/com/paulrezzonico/command/admin/UnMuteCommand.kt
@@ -0,0 +1,33 @@
+package com.paulrezzonico.command.admin
+
+import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import org.springframework.stereotype.Component
+
+@Component
+class UnMuteCommand : ListenerAdapter() {
+    override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
+        if (AdminPermissionUtils.isNotAdmin(event)) return
+        if (event.name != "unmute") return
+        handleUnMuteCommand(event)
+    }
+
+    private fun handleUnMuteCommand(event: SlashCommandInteractionEvent) {
+        val member = event.getOption("user")?.asMember
+        if (member == null) {
+            event.reply("User not found.").setEphemeral(true).queue()
+            return
+        }
+
+        val guild = event.guild ?: return
+        for (channel in guild.channels) {
+            if (channel is net.dv8tion.jda.api.entities.channel.concrete.TextChannel) {
+                channel.upsertPermissionOverride(member)
+                    .clear(Permission.MESSAGE_SEND)
+                    .queue()
+            }
+        }
+        event.reply("Unmuted ${member.asMention}.").queue()
+    }
+}
